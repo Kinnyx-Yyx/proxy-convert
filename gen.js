319e6b7acaf627e1a8b0033af8b7e9a279bb8133
Kinnyx-Yyx <kinnyx.yyx@gmail.com>
2024-08-25 01:15:52 +0000

    5

--- gen.js
+++ gen.js
@@ -16,7 +16,7 @@ const proxies = inputContents.map((line, index) => {
 
     return {
         name: `Proxy-${index + 1}`,
-        type: 'socks5', // http
+        type: 'http', // socks5
         server: server,
         port: parseInt(port, 10),
         username: username,
